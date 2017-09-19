886ab11d51ee9c371daf612240a7784d448999fb
Emma Makinson <emmamakinson@emmas-mbp.domain.name>
2017-09-19 14:19:01 +0100

    Initial Commit

---
+++ PinkPantherTests/PinkPantherTests.swift
@@ -0,0 +1,36 @@
+//
+//  PinkPantherTests.swift
+//  PinkPantherTests
+//
+//  Created by Emma Makinson on 19/09/2017.
+//  Copyright © 2017 Emma Makinson. All rights reserved.
+//
+
+import XCTest
+@testable import PinkPanther
+
+class PinkPantherTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
